f1c5fcb14123c2c1fd3babbeede308e737140378
floxydio <diooktar@gmail.com>
2022-09-16 18:53:34 +0700

    first commit

---
+++ lib/floxy_style.dart
@@ -0,0 +1,32 @@
+library floxy_style;
+
+import 'package:flutter/material.dart';
+
+class TombolMerah extends StatelessWidget {
+  final Widget widget;
+  final Function()? pressed;
+  final String typeBorder = "none";
+  const TombolMerah({Key? key, required this.widget, required this.pressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.redAccent,
+        shape: RoundedRectangleBorder(
+            borderRadius: typeBorder == "none"
+                ? const BorderRadius.all(Radius.circular(0))
+                : typeBorder == "bd-1"
+                    ? const BorderRadius.all(Radius.circular(10))
+                    : typeBorder == "bd-2"
+                        ? const BorderRadius.all(Radius.circular(20))
+                        : typeBorder == "bd-3"
+                            ? const BorderRadius.all(Radius.circular(30))
+                            : const BorderRadius.all(Radius.circular(0))),
+      ),
+      onPressed: pressed,
+      child: widget,
+    );
+  }
+}
